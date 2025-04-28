75d9794c1fc82ea5924ad63de21c240335538c45
kuanghl <kuanghl@kuanghl-pc>
2025-04-28 10:31:41 +0800

    feat(mermaid): add mermaid

---
+++ mdbook-mermaid/CHANGELOG.md
@@ -0,0 +1,122 @@
+# v0.15.0 (2025-03-28)
+
+* Upgrade to mermaid v11.2.0
+
+# v0.14.1 (2025-01-05)
+
+* Update dependencies
+* Add aarch64-unknown-linux-musl as a build target (Thanks, @sekhat)
+
+# v0.14.0 (2024-09-11)
+
+* Upgrade to mermaid v11.2.0
+
+# v0.13.0 (2023-12-13)
+
+* Update dependencies
+* Upgrade to mermaid v10.6.1
+
+# v0.12.6 (2022-12-19)
+
+* Fix issue with extracting the `dir` parameter on `mdbook-mermaid install`
+
+# v0.12.5 (2022-12-15)
+
+* Ensure the right features on dependencies are enabled
+
+# v0.12.4 (2022-12-15)
+
+* Update dependencies, including clap, to avoid breakage
+* Upgrade to mermaid v9.2.2
+
+# v0.12.3 (2022-12-06)
+
+* Use Ubuntu 20.04 when building `x86_64-unknown-linux-gnu` to avoid newer glibc
+
+# v0.12.2 (2022-11-29)
+
+* BUGFIX: Handle CRLF line endings in code block correctly ([#27](https://github.com/badboy/mdbook-mermaid/pull/27)).
+  Thanks, @pseiko1989.
+
+# v0.12.1 (2022-10-18)
+
+[Full changelog](https://github.com/badboy/mdbook-mermaid/compare/v0.12.0...v0.12.1)
+
+* BUGFIX: Handle arbitrary code span starts
+
+# v0.12.0 (2022-10-11)
+
+[Full changelog](https://github.com/badboy/mdbook-mermaid/compare/v0.11.2...v0.12.0)
+
+* Bump to mdbook 0.4.21
+* Dependency updates
+
+# v0.11.2 (2022-07-29)
+
+[Full changelog](https://github.com/badboy/mdbook-mermaid/compare/v0.11.1...v0.11.2)
+
+* Upgrade to mermaid v9.1.3
+
+# v0.11.1 (2022-07-16)
+
+[Full changelog](https://github.com/badboy/mdbook-mermaid/compare/v0.11.0...v0.11.1)
+
+* Reduce dependency tree by disabling default features of direct dependencies
+
+# v0.11.0 (2022-05-26)
+
+[Full changelog](https://github.com/badboy/mdbook-mermaid/compare/v0.10.0...v0.11.0)
+
+* Upgrade to mermaid v9.1.1
+* Upgrade dependencies
+
+# v0.10.0 (2022-02-07)
+
+[Full changelog](https://github.com/badboy/mdbook-mermaid/compare/v0.9.0...v0.10.0)
+
+* Upgrade to mermaid v8.13.10
+
+# v0.9.0 (2022-01-26)
+
+[Full changelog](https://github.com/badboy/mdbook-mermaid/compare/v0.8.3...v0.9.0)
+
+* Avoid roundtripping through pulldown-cmark.
+  Should make the produced markdown much more consistent.
+
+# v0.8.3 (2021-06-11)
+
+[Full changelog](https://github.com/badboy/mdbook-mermaid/compare/v0.8.2...v0.8.3)
+
+* Bump to mdbook v0.4.10
+
+# v0.8.2 (2021-06-11)
+
+[Full changelog](https://github.com/badboy/mdbook-mermaid/compare/v0.8.1...v0.8.2)
+
+* Bump to mdbook v0.4.9
+
+# v0.8.1 (2021-04-06)
+
+[Full changelog](https://github.com/badboy/mdbook-mermaid/compare/v0.8.0...v0.8.1)
+
+* Add tests to ensure `mdbook-mermaid install` works correctly
+
+# v0.8.0 (2021-02-09)
+
+[Full changelog](https://github.com/badboy/mdbook-mermaid/compare/v0.7.1...v0.8.0)
+
+* Upgrade to mermaid v8.9.0
+* Ensure additional files are added on `mdbook-mermaid install`
+
+# v0.7.1 (2021-01-07)
+
+[Full changelog](https://github.com/badboy/mdbook-mermaid/compare/v0.7.0...v0.7.1)
+
+* Fix Windows release assets
+
+# v0.7.0 (2021-01-06)
+
+[Full changelog](https://github.com/badboy/mdbook-mermaid/compare/v0.6.1...v0.7.0)
+
+* Upgrade to mermaid v8.8.4
+* Build against mdbook v0.4.5
---
+++ mdbook-mermaid/src/lib.rs
@@ -0,0 +1,299 @@
+use mdbook::book::{Book, BookItem, Chapter};
+use mdbook::errors::Result;
+use mdbook::preprocess::{Preprocessor, PreprocessorContext};
+use pulldown_cmark::{CodeBlockKind::*, Event, Options, Parser, Tag};
+
+pub struct Mermaid;
+
+impl Preprocessor for Mermaid {
+    fn name(&self) -> &str {
+        "mermaid"
+    }
+
+    fn run(&self, _ctx: &PreprocessorContext, mut book: Book) -> Result<Book> {
+        let mut res = None;
+        book.for_each_mut(|item: &mut BookItem| {
+            if let Some(Err(_)) = res {
+                return;
+            }
+
+            if let BookItem::Chapter(ref mut chapter) = *item {
+                res = Some(Mermaid::add_mermaid(chapter).map(|md| {
+                    chapter.content = md;
+                }));
+            }
+        });
+
+        res.unwrap_or(Ok(())).map(|_| book)
+    }
+
+    fn supports_renderer(&self, renderer: &str) -> bool {
+        renderer == "html"
+    }
+}
+
+fn escape_html(s: &str) -> String {
+    let mut output = String::new();
+    for c in s.chars() {
+        match c {
+            '<' => output.push_str("&lt;"),
+            '>' => output.push_str("&gt;"),
+            '"' => output.push_str("&quot;"),
+            '&' => output.push_str("&amp;"),
+            _ => output.push(c),
+        }
+    }
+    output
+}
+
+fn add_mermaid(content: &str) -> Result<String> {
+    let mut mermaid_content = String::new();
+    let mut in_mermaid_block = false;
+
+    let mut opts = Options::empty();
+    opts.insert(Options::ENABLE_TABLES);
+    opts.insert(Options::ENABLE_FOOTNOTES);
+    opts.insert(Options::ENABLE_STRIKETHROUGH);
+    opts.insert(Options::ENABLE_TASKLISTS);
+
+    let mut code_span = 0..0;
+    let mut start_new_code_span = true;
+
+    let mut mermaid_blocks = vec![];
+
+    let events = Parser::new_ext(content, opts);
+    for (e, span) in events.into_offset_iter() {
+        log::debug!("e={:?}, span={:?}", e, span);
+        if let Event::Start(Tag::CodeBlock(Fenced(code))) = e.clone() {
+            if &*code == "mermaid" {
+                in_mermaid_block = true;
+                mermaid_content.clear();
+            }
+            continue;
+        }
+
+        if !in_mermaid_block {
+            continue;
+        }
+
+        // We're in the code block. The text is what we want.
+        // Code blocks can come in multiple text events.
+        if let Event::Text(_) = e {
+            if start_new_code_span {
+                code_span = span;
+                start_new_code_span = false;
+            } else {
+                code_span = code_span.start..span.end;
+            }
+
+            continue;
+        }
+
+        if let Event::End(Tag::CodeBlock(Fenced(code))) = e {
+            assert_eq!(
+                "mermaid", &*code,
+                "After an opening mermaid code block we expect it to close again"
+            );
+            in_mermaid_block = false;
+
+            let mermaid_content = &content[code_span.clone()];
+            let mermaid_content = escape_html(mermaid_content);
+            let mermaid_content = mermaid_content.replace("\r\n", "\n");
+            let mermaid_code = format!("<div class=\"mermaid-container\" style=\"text-align: center\">\n<div class=\"mermaid\">{}</div>\n</div>\n\n", mermaid_content);
+            mermaid_blocks.push((span, mermaid_code));
+            start_new_code_span = true;
+        }
+    }
+
+    let mut content = content.to_string();
+    for (span, block) in mermaid_blocks.iter().rev() {
+        let pre_content = &content[0..span.start];
+        let post_content = &content[span.end..];
+        content = format!("{}\n{}{}", pre_content, block, post_content);
+    }
+    Ok(content)
+}
+
+impl Mermaid {
+    fn add_mermaid(chapter: &mut Chapter) -> Result<String> {
+        add_mermaid(&chapter.content)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use pretty_assertions::assert_eq;
+
+    use super::add_mermaid;
+
+    #[test]
+    fn adds_mermaid() {
+        let content = r#"# Chapter
+
+```mermaid
+graph TD
+A --> B
+```
+
+Text
+"#;
+
+        let expected = r#"# Chapter
+
+
+<pre class="mermaid">graph TD
+A --&gt; B
+</pre>
+
+
+
+Text
+"#;
+
+        assert_eq!(expected, add_mermaid(content).unwrap());
+    }
+
+    #[test]
+    fn leaves_tables_untouched() {
+        // Regression test.
+        // Previously we forgot to enable the same markdwon extensions as mdbook itself.
+
+        let content = r#"# Heading
+
+| Head 1 | Head 2 |
+|--------|--------|
+| Row 1  | Row 2  |
+"#;
+
+        let expected = r#"# Heading
+
+| Head 1 | Head 2 |
+|--------|--------|
+| Row 1  | Row 2  |
+"#;
+
+        assert_eq!(expected, add_mermaid(content).unwrap());
+    }
+
+    #[test]
+    fn leaves_html_untouched() {
+        // Regression test.
+        // Don't remove important newlines for syntax nested inside HTML
+
+        let content = r#"# Heading
+
+<del>
+
+*foo*
+
+</del>
+"#;
+
+        let expected = r#"# Heading
+
+<del>
+
+*foo*
+
+</del>
+"#;
+
+        assert_eq!(expected, add_mermaid(content).unwrap());
+    }
+
+    #[test]
+    fn html_in_list() {
+        // Regression test.
+        // Don't remove important newlines for syntax nested inside HTML
+
+        let content = r#"# Heading
+
+1. paragraph 1
+   ```
+   code 1
+   ```
+2. paragraph 2
+"#;
+
+        let expected = r#"# Heading
+
+1. paragraph 1
+   ```
+   code 1
+   ```
+2. paragraph 2
+"#;
+
+        assert_eq!(expected, add_mermaid(content).unwrap());
+    }
+
+    #[test]
+    fn escape_in_mermaid_block() {
+        let _ = env_logger::try_init();
+        let content = r#"
+```mermaid
+classDiagram
+    class PingUploader {
+        <<interface>>
+        +Upload() UploadResult
+    }
+```
+
+hello
+"#;
+
+        let expected = r#"
+
+<pre class="mermaid">classDiagram
+    class PingUploader {
+        &lt;&lt;interface&gt;&gt;
+        +Upload() UploadResult
+    }
+</pre>
+
+
+
+hello
+"#;
+
+        assert_eq!(expected, add_mermaid(content).unwrap());
+    }
+
+    #[test]
+    fn more_backticks() {
+        let _ = env_logger::try_init();
+        let content = r#"# Chapter
+
+````mermaid
+graph TD
+A --> B
+````
+
+Text
+"#;
+
+        let expected = r#"# Chapter
+
+
+<pre class="mermaid">graph TD
+A --&gt; B
+</pre>
+
+
+
+Text
+"#;
+
+        assert_eq!(expected, add_mermaid(content).unwrap());
+    }
+
+    #[test]
+    fn crlf_line_endings() {
+        let _ = env_logger::try_init();
+        let content = "# Chapter\r\n\r\n````mermaid\r\n\r\ngraph TD\r\nA --> B\r\n````";
+        let expected =
+            "# Chapter\r\n\r\n\n<pre class=\"mermaid\">\ngraph TD\nA --&gt; B\n</pre>\n\n";
+
+        assert_eq!(expected, add_mermaid(content).unwrap());
+    }
+}
